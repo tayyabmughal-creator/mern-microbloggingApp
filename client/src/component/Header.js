eb4adc0ba740eb0ebd3afb72ca776d9717e84d73
hari <78838953+hari998@users.noreply.github.com>
2022-10-02 01:48:37 +0530

    Create Header.js

---
+++ client/src/component/Header.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import {Navigationbar} from "./Navigationbar";
+
+export function Header() {
+  return (
+    <>
+    <Navigationbar />
+    </>
+  )
+}
+
